d1d37ad3d3c5f50f2d8e4ec02dad99e6673b144f
Gloria <gloghe@yahoo.it>
2022-06-08 15:03:21 +0200

    section

--- src/components/SectionDC.vue
+++ src/components/SectionDC.vue
@@ -1,6 +1,6 @@
 <template>
    <section>
-       
+       <h2> Content goes here... </h2>
    </section>
 </template>
 
@@ -8,14 +8,21 @@
 export default {
     name: 'SectionDC',
     
-    
-
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+section{
+    background-color: #1c1c1c;
+    color: white;
+    //debugger
+    height: 130px;
 
+    h2{
+        padding-top: 40px;
+    }
+}
 
 
 </style>
--- src/components/SectionLink.vue
+++ src/components/SectionLink.vue
@@ -1,21 +1,58 @@
 <template>
-   <section>
-       
-   </section>
+    <section>
+        <div v-for="(element, i) in buy" :key="i">
+            <a :href="element.link">
+                <img :src="element.img" :alt="element.name">
+                <span>{{element.name}}</span>
+            </a>
+
+        </div>
+    </section>
 </template>
 
 <script>
+
 export default {
     name: 'SectionLink',
-    
-    
+    data() {
+        return {
+            buy: [
+                {
+                    img: "../assets/img/buy-comics-digital-comics.png",
+                    name: "DIGITAL COMICS",
+                    link: "#"
+                },
+                {
+                    img: "../assets/img/buy-comics-digital-merchandise.png",
+                    name: "DC MERCHANDISE",
+                    link: "#"
+                },
+                {
+                    img: "../assets/img/buy-comics-shop-locator.png",
+                    name: "SUBSCRIPTION",
+                    link: "#"
+                },
+                {
+                    img: "../assets/img/buy-comics-subscriptions.png",
+                    name: "COMIC SHOP LOCATOR",
+                    link: "#"
+                },
+                {
+                    img: "../assets/img/buy-dc-power-visa.svg",
+                    name: "DC POWER VISA",
+                    link: "#"
+                }
+            ]
+        }
+    }
+
 
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
-
-
+section {
+    background-color: #0282f9;
+}
 </style>
