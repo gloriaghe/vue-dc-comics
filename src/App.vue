7251d6931dd4ac2a73ae8d0e3b7ec1d92156802e
Gloria <gloghe@yahoo.it>
2022-06-08 14:12:30 +0200

    primo push

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    
+    <HeaderDC/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderDC from './components/HeaderDC.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    HeaderDC,
   }
 }
 </script>
---
+++ src/components/HeaderDC.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <a href="#">
+            <img src="../assets/img/dc-logo.png" alt="Logo Dc Comics">
+        </a>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderDC',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
